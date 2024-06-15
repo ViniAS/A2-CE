f8b40c9d4e1a96613b90c6a2db90199db500e389
Vinícius Sousa <vinicius.antunes@proton.me>
2024-06-15 17:43:13 -0300

    optimize server

--- src/mock_server.py
+++ src/mock_server.py
@@ -1,6 +1,10 @@
 from celery import Celery
+import psycopg2
+from psycopg2 import pool
 import json
 from pyspark.sql import SparkSession
+# Run with "celery -A mock_server worker --loglevel=INFO --concurrency=10"
+
 
 app = Celery('tasks', broker='pyamqp://guest@localhost//')
 
@@ -14,26 +18,38 @@ jdbc_driver_path = "../jdbc/postgresql-42.7.3.jar"
 db_properties = {
     "user": config['db_user'],
     "password": config['db_password'],
-    "driver": "org.postgresql.Driver",
-    "url": config['db_url']
+    "host": "localhost",
+    "port": "5432",
+    "dbname": "source_db"
 }
 
+db_pool = psycopg2.pool.SimpleConnectionPool(1, 20, **db_properties)
 
 
 @app.task
 def store_user_behavior(message):
-    # Cria uma sessão Spark
-    spark = SparkSession.builder \
-        .appName("Store Log") \
-        .config("spark.jars", jdbc_driver_path) \
-        .config("spark.ui.port", "4050") \
-        .getOrCreate()
+    # Get a connection from the pool
+    conn = db_pool.getconn()
 
-    df = spark.createDataFrame([message])
+    try:
+        # Open a cursor to perform database operations
+        cur = conn.cursor()
 
-    df.write.jdbc(url=db_properties['url'], table="log_user_behavior",
-                  mode="append", properties=db_properties)
+        # Execute a query
+        cur.execute(
+            "INSERT INTO log_user_behavior (user_author_id, action, button_product_id, stimulus, component, text_content) VALUES (%s, %s, %s, %s, %s, %s)",
+            (message['user_author_id'], message['action'], message['button_product_id'], message['stimulus'],
+             message['component'], message['text_content'])
+        )
 
-    spark.stop()
+        # Commit the transaction
+        conn.commit()
+
+    finally:
+        # Close the cursor and the connection (returns it to the pool)
+        if cur is not None:
+            cur.close()
+        db_pool.putconn(conn)
+
 
 
