94fdea1cad98af30cc859b41ee49231070b83c24
Otávio Augusto <otavio.oam@protonmail.com>
2024-06-17 09:50:30 -0300

    fix extrator of logs

--- src/extractor_log.py
+++ src/extractor_log.py
@@ -1,7 +1,9 @@
-# Usa o Spark para acessar um diretório (s3 ou local) e extrair os arquivos de log dele e enviar para uma queue RabbitMQ
+# Usa o Spark para acessar um diretório (s3 ou local) e extrair os arquivos de log dele e enviar para o 
+# banco de dados PostgreSQL Target
 
 import pyspark.sql.functions as F
 from pyspark.sql import SparkSession
+import json
 
 # Path para o diretório de logs
 log_dir = "../data/logs"
@@ -13,9 +15,34 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 # Lê os dados de log
-log_df = spark.read.text(log_dir)
+log_user_behavior = spark.read.text(log_dir+"/user_behavior", header=["user_author_id", "action", 
+                                                                      "button_product_id", "stimulus", 
+                                                                      "component", "text_content", "date"])
 
-# printa os dados
-log_df.show()
+log_debug = spark.read.text(log_dir+"/debug", header=["message", "text_content", "date"])
 
-# TODO: Implementar o código para enviar os dados para uma fila RabbitMQ
+log_failure = spark.read.text(log_dir+"/failure", header=["component", "severity", "message", 
+                                                          "text_content", "date"])
+
+log_audit = spark.read.text(log_dir+"/audit", header=["user_author_id", "action", "action_description", 
+                                                      "text_content", "date"])
+
+# Load configuration from config.json
+with open('config.json') as f:
+    config = json.load(f)
+
+# Path to the PostgreSQL JDBC driver
+jdbc_driver_path = "../jdbc/postgresql-42.7.3.jar"
+
+url = config['db_target_url']
+db_properties = {
+    "user": config['db_target_user'],
+    "password": config['db_target_password'],
+    "driver": "org.postgresql.Driver"
+}
+
+#writes the data to a PostgreSQL database
+log_user_behavior.write.jdbc(url=url, table="log_user_behavior", mode="overwrite", properties=db_properties)
+log_debug.write.jdbc(url=url, table="log_debug", mode="overwrite", properties=db_properties)
+log_failure.write.jdbc(url=url, table="log_failure", mode="overwrite", properties=db_properties)
+log_audit.write.jdbc(url=url, table="log_audit", mode="overwrite", properties=db_properties)
--- src/pre_process_log.py
+++
@@ -1,1 +0,0 @@
-# connects in the queue of log data preprocess it and send to a another queue
