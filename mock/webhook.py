c96101f8057a0e56f5178b68aa54411333a247e3
Vinícius Sousa <vinicius.antunes@proton.me>
2024-06-18 23:30:13 -0300

    Update webhook.py

---
+++ mock/webhook.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+
+from src.webhook_queue import store_user_behavior
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        print(request.json)
+        store_user_behavior.delay(request.data)
+        return {'status': 'success'}, 200
+    
+    else:
+        return {'status': 'error'}, 405
+
+if __name__ == '__main__':
+    app.run(port=5000)
