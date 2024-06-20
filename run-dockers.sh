e0174660290b77715bfe2902c8f91d8720adc36e
Vinícius Sousa <vinicius.antunes@proton.me>
2024-06-20 20:14:54 -0300

    docker compose working

--- run-dockers.sh
+++ run-dockers.sh
@@ -2,17 +2,26 @@
 docker network create net
 
 # PostgreSQL
-docker run --name psql_container -d --net net -p 5432:5432 -d psql
+docker run --name psql -d --net net -p 5432:5432 -d psql
+
+# Broker
+docker run --name broker --net net -p 5672:5672 -p 15672:15672 -d broker
+
+# Queue
+docker run --name queue --net net -d queue
 
 # Historico Mock
-docker run  --name historico_container --net net -d historico
+docker run  --name historico --net net -d historico
 
 # Loja Mock
-docker run --name loja_container --net net -d loja
+docker run --name loja --net net -d loja
 
 # Webhook
-docker run --name webhook_container --net net -d webhook
+docker run --name webhook --net net -p 55555:55555 -d webhook 
+
+# Web Gen
+docker run --name web_gen --net net -d web_gen
 
 # Dashboard 
-docker run --name dash_container --net net -d dash
+docker run --name dash --net net -d dash
 
