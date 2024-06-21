e6f7abdb8e49d243417e58c176588fedd34fd3d2
Vinícius Sousa <vinicius.antunes@proton.me>
2024-06-20 22:50:35 -0300

    bonificador docker

--- build-dockers.sh
+++ build-dockers.sh
@@ -1,19 +1,6 @@
 
 docker build -t psql -f ./Dockerfile.psql .
 
-docker build -t historico -f ./Dockerfile.historico .
-
-docker build -t loja -f ./Dockerfile.loja .
-
-docker build -t dash -f ./Dockerfile.dash .
-
-docker build -t txt -f ./Dockerfile.txt_behaviour .
-
-docker build -t queue -f ./Dockerfile.queue .
-
-docker build -t webhook -f ./Dockerfile.webhook .
-
-docker build -t web_gen -f ./Dockerfile.web_gen .
-
 docker build -t broker -f ./Dockerfile.broker .
+
 
